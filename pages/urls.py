8c439c8a666cec459e63347eabdf4b94cc2be157
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-08 15:27:30 -0400

    pt.6

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 
-from .views import AboutUsView, HomePageView, MathFactsView, MyAccountView
+from .views import AboutUsView, HomePageView, MyAccountView
 
 app_name = 'pages'
 urlpatterns = [
     path('', HomePageView.as_view(), name='homepage'),
     path('about-us/', AboutUsView.as_view(), name='about-us'),
-    path('math_scorelist/', MathFactsView.as_view(), name='math-scorelist'),
     path('my-account/', MyAccountView.as_view(), name ='my-account'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,8 +6,5 @@ class HomePageView(TemplateView):
 class AboutUsView(TemplateView):
     template_name = 'pages/about_us.html'
 
-class MathFactsView(TemplateView):
-    template_name = 'pages/math_scorelist.html'
-
 class MyAccountView(TemplateView):
     template_name = "account/my_account.html" 
