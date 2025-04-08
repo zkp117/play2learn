147953e4d9b85cdf676cefd4cd4850760dab4c8b
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-08 15:44:42 -0400

    pt.8

--- play2learn/urls.py
+++ play2learn/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
     path('', include("games.urls")),
     path('',include("pages.urls")),
