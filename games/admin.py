65fe0573e07d7ef8d4d24a2d378fc278f6c0c1fb
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-11 11:30:22 -0400

    finished

--- games/admin.py
+++ games/admin.py
@@ -5,22 +5,22 @@ from .models import MathFactsScore, AnagramHuntScore
 @admin.register(MathFactsScore)
 class MathFactsScoresAdmin(admin.ModelAdmin):
     model = MathFactsScore
-    list_display = ['user', 'score', 'created_at']
+    list_display = ['user', 'score', 'date']  # Use 'date' instead of 'created_at'
 
     # Prevent editing of fields when editing an existing record
     def get_readonly_fields(self, request, obj=None):
         if obj:  # If it's an existing object, make the fields read-only
-            return ('user', 'score', 'created_at')
+            return ('user', 'score', 'date')
         return ()  # Fields are editable when creating new records
 
 # AnagramHunt Scores Admin
 @admin.register(AnagramHuntScore)
 class AnagramHuntScoresAdmin(admin.ModelAdmin):
     model = AnagramHuntScore
-    list_display = ['user', 'score', 'created_at']
+    list_display = ['user', 'score', 'date']  # Use 'date' instead of 'created_at'
 
     # Prevent editing of fields when editing an existing record
     def get_readonly_fields(self, request, obj=None):
         if obj:  # If it's an existing object, make the fields read-only
-            return ('user', 'score', 'created_at')
+            return ('user', 'score', 'date')
         return ()  # Fields are editable when creating new records
