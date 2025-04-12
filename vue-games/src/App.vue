86726d358d1c4592037c8d6e4bef39b08039aed9
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-12 12:53:04 -0400

    putting in games

--- vue-games/src/App.vue
+++ vue-games/src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <router-view/>
+  <MathFacts />
+  <AnagramHunt />
 </template>
+
+<script>
+import MathFacts from './components/MathFacts.vue';
+import AnagramHunt from './components/AnagramHunt.vue';
+
+export default {
+name: 'App',
+components: {
+  AnagramHunt,
+  MathFacts,
+}
+}
+</script>
