6b08ea89162a53835be0982c56bf3f7fb4486d9b
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-12 14:00:57 -0400

    pt.9

--- users/admin.py
+++ users/admin.py
@@ -8,20 +8,30 @@ from .models import CustomUser
 CustomUser = get_user_model()
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+
     model = CustomUser
 
-    readonly_fields = ['password_form', 'get_anagramhunt_scores', 'get_mathfacts_scores']
+    readonly_fields = ['password_form', 'get_anagramhunt_scores', 'get_mathfacts_scores', 'avatar_display']
 
     list_display = UserAdmin.list_display + ('is_superuser', 'get_anagramhunt_scores', 'get_mathfacts_scores',)
     list_display_links = ('username', 'email', 'first_name', 'last_name')
 
     fieldsets = UserAdmin.fieldsets + (
         ('Game Scores', {'fields': ('get_anagramhunt_scores', 'get_mathfacts_scores')}),
+        ('Personal Information', {'fields': ('dob', 'avatar')}),  # Added avatar and dob fields
     )
 
     def password_form(self, obj):
         url = reverse('admin:auth_user_password_change', args=[obj.pk])
         return mark_safe(f'<a href="{url}">Change Password</a>')
+    
+    def avatar_display(self, obj):
+        if obj.avatar:
+            return mark_safe(f'<img src="{obj.avatar.url}" width="50" height="50"/>')
+        return "No Avatar"
+
+    avatar_display.short_description = 'Avatar'
+
 
     def get_anagramhunt_scores(self, obj):
         scores = obj.anagram_scores.all()
