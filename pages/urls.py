f8408257918b1e7837c7dee40e4d593e31fe64fc
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-12 14:16:03 -0400

    pt.11

--- pages/urls.py
+++ pages/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from .views import AboutUsView, HomePageView, MyAccountView, AnagramHuntView, MathFactsView
+from .views import AboutUsView, HomePageView, AnagramHuntView, MathFactsView
 
 app_name = 'pages'
 urlpatterns = [
     path('', HomePageView.as_view(), name='homepage'),
     path('about-us/', AboutUsView.as_view(), name='about-us'),
-    path('my-account/', MyAccountView.as_view(), name ='my-account'),
     path('anagram-hunt/', AnagramHuntView.as_view(), name='anagram-hunt'),
     path('math-facts/', MathFactsView.as_view(), name='math-facts')
 ]
