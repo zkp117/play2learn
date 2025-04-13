e58f3b05d72b18bcf1845ecc8082f6dedda21630
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-13 17:08:20 -0400

    pt.24

---
+++ vue-games/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <AnagramHunt />
+  <MathFacts />
+</template>
+
+<script>
+import AnagramHunt from './apps/AnagramHunt.vue';
+import MathFacts from './apps/MathFacts.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MathFacts,
+    AnagramHunt
+  }
+}
+</script>
+
+<style>
+
+</style>
