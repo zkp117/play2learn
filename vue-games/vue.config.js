338ca3b278f4c6e295377eb51a451f56934e903e
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-13 14:41:18 -0400

    pt.21

--- vue-games/vue.config.js
+++ vue-games/vue.config.js
@@ -1,11 +1,12 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
 module.exports = {
-  publicPath: 'http://localhost:8080', // The base URL where your app will be deployed 
+  publicPath: 'http://localhost:8080', // The base URL where your app will be deployed
   outputDir: '../static/dist', // The path for where files will be output when the app is built
   indexPath: '../../templates/_base_vue.html', // The path for the generated index file
+
+  transpileDependencies: [
+    // List the dependencies that need to be transpiled here
+    'vue-router',  // Example dependency, add any that you need
+  ],
 
   configureWebpack: {
     devServer: {
