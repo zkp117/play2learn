2bffd72791d30721883619900c2591ce67ae934c
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-16 12:09:54 -0400

    pt.7

--- reviews/forms.py
+++ reviews/forms.py
@@ -4,6 +4,19 @@ GAME_CHOICES = {
     'Anagram Hunt',
     'Math Facts',
 }
+
+WORDLENGTH_REVIEW_CHOICES = {
+    '5',
+    '6',
+    '7',
+    '8',
+}
+
+MATHLEVEL_REVIEW_CHOICES = {
+    [1-100]
+}
+
+
 class ReviewsForm(forms.Form):
     email = forms.EmailField()
     game_choices = [...]
