6c35ee909cfb71c4aca0052b5d0ac67e12840b99
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-16 11:43:30 -0400

    pt.4

--- reviews/forms.py
+++ reviews/forms.py
@@ -4,10 +4,10 @@ GAME_CHOICES = {
     'Anagram Hunt',
     'Math Facts',
 }
-
 class ReviewsForm(forms.Form):
     email = forms.EmailField()
     game_choices = [...]
     user_review = [...]
+    review_viewability = [...]
 
 
--- reviews/views.py
+++ reviews/views.py
@@ -1,12 +1,30 @@
+import html
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
 
+from common.utils.email_service import send_email
 from .forms import ReviewsForm
-
 class ReviewsAppView(FormView):
     template_name = 'reviews/write_views.html'
     form_class = ReviewsForm
     success_url = reverse_lazy('reviews:thanks')
+
+    def form_valid(self, form):
+        data = form.cleaned_data
+        to = 'neeneez2008@gmail.com'
+        subject = 'Application for Joke Writer'
+        content = f'''<p>Your {'game_choices'} review</p>
+            <p>Game review received:</p>
+            <ol>'''
+        for key, value in data.items():
+            label = key.replace('_', ' ').title()
+            entry = html.escape(str(value), quote=False)
+            content += f'<li>{label}: {entry}</li>'
+        
+        content += '</ol>'
+
+        send_email(to, subject, content)
+        return super().form_valid(form)
 
 class ReviewsAppThanksView(TemplateView):
     template_name = 'reviews/thanks.html'
