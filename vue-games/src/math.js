5c5bcf4e40094a49bf3b88bebeff688ce44f3e47
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-17 11:40:09 -0400

    pt.2

--- vue-games/src/math.js
+++ vue-games/src/math.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import MathFacts from './MathFacts.vue'
+import { createApp } from 'vue';
+import MathFacts from './MathFacts.vue';
+import router from './router';
 
-createApp(MathFacts).mount('#math-facts')
+createApp(MathFacts).mount('#math-facts');
+app.use(router);
+app.mount('#math-app');
