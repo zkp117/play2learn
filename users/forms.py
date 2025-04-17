77a76ddc4abe0fd3c7c058547a9341ed47441ce2
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-17 11:15:03 -0400

    pt.12

--- users/forms.py
+++ users/forms.py
@@ -15,8 +15,7 @@ class CustomUserChangeForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ('email', 'username', 'first_name', 'last_name', 'dob', 'avatar',
-                  'anagramhunt_score', 'mathfacts_score', 'mathfacts_review', 
-                  'anagramhunt_review')
+                'mathfacts_review', 'anagramhunt_review')
         widgets = {
             'dob': forms.SelectDateWidget(
                 attrs={
