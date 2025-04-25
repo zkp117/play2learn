eb1c57d4af9e7d481f4b45475f4026e749cd44b4
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-25 15:52:27 -0400

    pt.18

--- contact/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class ContactStaffForm(forms.Form):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    email = forms.EmailField()
--- games/views.py
+++
@@ -1,5 +0,0 @@
-from django.views.generic import TemplateView
-class MathFactsView(TemplateView):
-    template_name = "games/math-facts.html"
-class AnagramHuntView(TemplateView):
-    template_name = "games/anagram-hunt.html"
