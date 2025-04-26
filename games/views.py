a783ca80da7d0dae195b2c66ed1e043d34da508d
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-26 16:57:38 -0400

    pt.13

--- games/views.py
+++ games/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 class MathFactsView(TemplateView):
     template_name = "vue-templates/math-facts.html"
+
 class AnagramHuntView(TemplateView):
     template_name = "vue-templates/anagram-hunt.html"
--- play2learn/settings.py
+++ play2learn/settings.py
@@ -74,7 +74,9 @@ ROOT_URLCONF = 'play2learn.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates',
+                 BASE_DIR / 'play2learn/templates/vue-templates',
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
