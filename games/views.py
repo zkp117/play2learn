78ae8d28432c8f77955ae540b5b4431f4767afd1
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-26 15:32:19 -0400

    pt.7

--- games/views.py
+++ games/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
 class MathFactsView(TemplateView):
-    template_name = "games/math-facts.html"
+    template_name = "vue-templates/math-facts.html"
 class AnagramHuntView(TemplateView):
-    template_name = "games/anagram-hunt.html"
+    template_name = "vue-templates/anagram-hunt.html"
