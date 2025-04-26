261a18c17dd02b8a37b20a303cc5fc417244fbf0
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-26 15:34:59 -0400

    pt.9

--- vue-games/vue.config.js
+++ vue-games/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   publicPath: '/', // Or use '/vue-games/' if deploying in a subdirectory
   outputDir: 'dist', // Direct output to the 'dist' folder within vue-games
   // Remove or adjust indexPath to avoid generating _base_vue.html
-  indexPath: '../templates/index.html', // or adjust this path as needed
+  indexPath: 'templates/index.html', // or adjust this path as needed
 
   configureWebpack: {
     devServer: {
