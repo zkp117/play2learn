86a150b5bb8c4ea71ae7c5c1cadc64906c975f0c
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-27 11:00:49 -0400

    setting up math review pt.1

--- play2learn/urls.py
+++ play2learn/urls.py
@@ -4,8 +4,9 @@ from django.urls import path, include
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
-    path('', include("games.urls")),
-    path('',include("pages.urls")),
-    path('', include('users.urls')),
-    path('reviews/',include('reviews.urls')),
+    path("games/", include("games.urls")),
+    path("pages/", include("pages.urls")),
+    path("users/", include("users.urls")),
+    path('reviews/', include('reviews.urls')),
 ]
+
