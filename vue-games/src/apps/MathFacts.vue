85e011a9f6653a56fa14a1683c0eabffcf02c777
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-27 14:35:27 -0400

    pt.7

--- vue-games/src/apps/MathFacts.vue
+++ vue-games/src/apps/MathFacts.vue
@@ -99,8 +99,7 @@
       <div class="row d-flex flex-col text-center">
         <button @click="play" class="btn btn-primary w-100 m-1">Play Again</button>
         <button @click="screen = 'start'" class="btn btn-secondary w-100 m-1">Back to Start Screen</button>
-        <a href="{% url 'vue-reviews:reviewing_math' %}" class="btn btn-info w-100 m-1">Write MathFacts Review</a>
-
+        <button @click="reviewMath" class="btn btn-info w-100 m-1">Write MathFacts Review</button>
       </div>
     </div>
   </div>
@@ -118,6 +117,7 @@ label {
 </style>
 
 <script>
+
 import { getRandomInteger } from '@/helpers/helpers';
 
 export default {
@@ -164,6 +164,9 @@ export default {
         this.number1 = num1;
         this.number2 = num2;
       }
+    },
+    reviewMath(){
+      this.$router.push({name: 'vue-reviews:reviewing_math'});
     },
     async recordScore() {
       // TODO: when Math Facts finishes, make an Ajax call with axios (this.axios)
