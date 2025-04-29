5adba1703cc7dac381d58b878b2483bfa783c017
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-04-29 13:36:27 -0400

    pt.8

--- scoreboards/views.py
+++ scoreboards/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 class AllUsersMathScores(TemplateView):
-    template_name = 'vue-scorebaords/scoreboard_math.html'
+    template_name = 'vue-scoreboards/scoreboard_math.html'
 class AllUsersAnagramScores(TemplateView):
-    template_name = 'vue-scorebaords/scoreboard_anagrams.html'
+    template_name = 'vue-scoreboards/scoreboard_anagrams.html'
 
