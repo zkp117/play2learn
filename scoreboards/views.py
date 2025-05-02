99599e2708923888c8a570a70966741b7c285157
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-05-02 12:46:46 -0400

    pt.3

--- scoreboards/views.py
+++ scoreboards/views.py
@@ -1,8 +1,20 @@
 from django.views.generic import TemplateView
+from .models import MathFactsScoreBoard, AnagramHuntScoreBoard
 class ScoreBoards(TemplateView):
     template_name = 'templates/scoreboards.html'
 
-    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # these display the top 10 scores for the mathfacts + anagramhunt scoreboards
+
+        context['mathfacts_scores'] = MathFactsScoreBoard.objects.order_by('-score')[:10]
+
+        context['anagramhunt_scores'] = AnagramHuntScoreBoard.objects.order_by('-score')[:10]
+
+        return context
+
+
 
 
 
