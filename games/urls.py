54f88cdd49df7277e7accd4b2212636b9eb37ac9
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-05-05 11:44:43 -0400

    pt.18

--- games/urls.py
+++ games/urls.py
@@ -1,11 +1,8 @@
-app_name = 'games'
-
 from django.urls import path
 from .views import MathFactsView, AnagramHuntView, EnterMathFactsScore
 
 urlpatterns = [
     path('math-facts/', MathFactsView.as_view(), name='math-facts'),
     path('anagram-hunt/', AnagramHuntView.as_view(), name='anagram-hunt'),
-
     path("api/record-score/mathfacts/", EnterMathFactsScore.as_view(), name="record_mathfacts_score"),
 ]
--- play2learn/urls.py
+++ play2learn/urls.py
@@ -1,17 +1,15 @@
-# play2learn/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-
-    # Add vue-games prefix to game URLs
-    path("vue-games/anagram-hunt/", include('games.urls', namespace='games')),  # Anagram Hunt game
-    path("vue-games/math-facts/", include('games.urls', namespace='games')),  # Math Facts game
-
-    # Other paths
     path("accounts/", include("allauth.urls")),
-    path('', include('pages.urls')),
+    path("admin/", admin.site.urls),
+    
+    # Corrected include for 'games' app with the 'app_name' and 'namespace'
+    path('games/', include(('games.urls', 'games'), namespace='games')),
+    
+    # Include other apps with clear paths
+    path('', include("pages.urls")),
     path('', include('users.urls')),
     path('', include('reviews.urls')),
     path('scoreboards/', include('scoreboards.urls')),
