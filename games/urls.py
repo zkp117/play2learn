7fdaecd458e66545c6d33e14829e589cee0acab0
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-05-06 15:17:45 -0400

    pt.5

--- games/urls.py
+++ games/urls.py
@@ -5,6 +5,6 @@ from .views import MathFactsView, AnagramHuntView, EnterMathFactsScore
 
 urlpatterns = [
     path('math-facts/', MathFactsView.as_view(), name='math-facts'),
-    path('anagram-hunt/', AnagramHuntView.as_view(), name='anagram-hunt'),
+    path('vue-games/anagram-hunt/', AnagramHuntView.as_view(), name='anagram-hunt'),
     path("api/record-score/mathfacts/", EnterMathFactsScore.as_view(), name="record_mathfacts_score"),
 ]
--- games/views.py
+++ games/views.py
@@ -1,7 +1,7 @@
 from .models import MathFactsScore, AnagramHuntScore
 from django.views.generic import TemplateView
-from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.views import View
@@ -11,15 +11,15 @@ class MathFactsView(TemplateView):
 
 class AnagramHuntView(TemplateView):
     template_name = "vue-templates/anagram-hunt.html"
-@method_decorator(login_required, name='dispatch')
+@method_decorator([login_required, never_cache], name='dispatch')
 class EnterMathFactsScore(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
             score = data.get('score')
             if score is not None:
-                MathFactsScore.objects.create(user=request.user, score=score)
-                return JsonResponse({'status': 'success', 'score': score})
+                    MathFactsScore.objects.create(user=request.user, score=score)
+                    return JsonResponse({'status': 'success', 'score': score})
             else:
                 return JsonResponse({'status': 'error', 'message': 'No score provided'}, status=400)
         except json.JSONDecodeError:
