3e8a57fad9bb889f07bd79b102042885349ef90a
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-05-09 12:25:21 -0400

    fixing my_account pt.1

--- users/views.py
+++ users/views.py
@@ -48,6 +48,7 @@ class MyAccountPageView( SuccessMessageMixin, LoginRequiredMixin, UpdateView):
         response = super().form_valid(form)
         self.request.user.refresh_from_db()
         return response
+
     
 @login_required
 def clear_avatar(request):
