238f3df56774e264483ac98cf6142c34205a75c2
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-05-13 14:44:22 -0400

    pt.13

--- scoreboards/views.py
+++ scoreboards/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-from scoreboards.models import MathFactsScoreBoard, AnagramHuntScoreBoard
+from .models import MathFactsScoreBoard, AnagramHuntScoreBoard
 class ScoreBoards(TemplateView):
     template_name = 'scoreboards.html'
 
--- vue-games/src/apps/AnagramHunt.vue
+++ vue-games/src/apps/AnagramHunt.vue
@@ -58,7 +58,7 @@
         <button @click="play" class="btn btn-primary w-100 m-1">Play Again</button>
         <button @click="screen = 'start'" class="btn btn-secondary w-100 m-1">Back to Start Screen</button>
         <button onclick="window.location.href='/reviewing_anagram/'" class="btn btn-info w-100 m-1">Write AnagramHunt Review</button>
-        <button onclick="window.location.href='[...]'" class="btn btn-light w-100 m-1">Check the Scoreboard</button>
+        <button onclick="window.location.href='/scoreboards/'" class="btn btn-light w-100 m-1">Check the Scoreboard</button>
       </div>
     </div>
   </div>
