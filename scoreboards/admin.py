3e898f6d6e28224b6d95bd4df1b2768f7a79cb51
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-05-13 08:56:54 -0400

    pt.9

--- scoreboards/admin.py
+++ scoreboards/admin.py
@@ -1,16 +1,10 @@
 from django.contrib import admin
 from .models import MathFactsScoreBoard, AnagramHuntScoreBoard
-
-# MathFacts ScoreBoard Admin
 @admin.register(MathFactsScoreBoard)
 class MathFactsScoreBoardAdmin(admin.ModelAdmin):
     model = MathFactsScoreBoard
-    list_display = ['score', 'user', 'date_added']  # Add your fields
-    # Customize other settings here
-
-# AnagramHunt ScoreBoard Admin
+    list_display = ['score', 'user', 'date_added']
 @admin.register(AnagramHuntScoreBoard)
 class AnagramHuntScoreBoardAdmin(admin.ModelAdmin):
     model = AnagramHuntScoreBoard
-    list_display = ['score', 'user', 'date_added']  # Add your fields
-    # Customize other settings here
+    list_display = ['score', 'user', 'date_added']
