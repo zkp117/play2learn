bc80d492688d3938504deadef0492b74e0b548b5
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-05-21 14:03:33 -0400

    pt.5

--- contact/forms.py
+++ contact/forms.py
@@ -4,3 +4,7 @@ class ContactStaffForm(forms.Form):
     first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.EmailField()
+    user_input = forms.CharField(
+        label = 'Write your question or comment here!',
+        widget=forms.Textarea
+    )
