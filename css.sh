e4169c211d2c0c0e2515454b899cec8baecf914d
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-05-24 12:29:58 -0400

    pt.15

--- css.sh
+++ css.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+#!/bin/bash
+
 echo "Uploading scoreboard CSS to S3..."
-aws s3 cp ./static/css/style.css s3://play2learn-bucket/static/css/style.css --acl public-read --content-type "text/css"
+aws s3 cp ./static/css/style.css s3://play2learn-bucket/css/style.css --acl public-read --content-type "text/css"
 echo "CSS upload complete."
 
 
 # open this file in terminal
 # run (first) 'chmod +x css.sh'
 # run (second) './css.sh'
-# do NOT run this script unless you make changes to 'static/css/styles.css'
+# do NOT run this script unless you make changes to './css/styles.css'
