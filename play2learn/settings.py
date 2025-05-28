833c79265d652398937114bb35d43223884c4fde
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-05-28 18:28:11 -0400

    pt.10

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -113,6 +113,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 DEFAULT_FROM_EMAIL = 'neeneez2008@gmail.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 ACCOUNT_SIGNUP_FORM_CLASS = 'users.forms.SignupForm'
 
