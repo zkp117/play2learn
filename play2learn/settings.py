1c404313e068de9e255958ebd4d9946b97ddafbc
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-05-30 20:08:35 -0400

    pt.17

--- play2learn/settings.py
+++ play2learn/settings.py
@@ -125,17 +125,6 @@ DATABASES = {
     )
 }
 
-# DATABASES = {
-  # 'default': {  
-  #     'ENGINE': 'django.db.backends.postgresql',
-  #     'NAME': 'user_info',
-   #    'USER': 'zoeparigian',
-   #    'PASSWORD': 'Pandora117!',
-  #     'HOST': 'localhost',
-   #    'PORT': 6623
-  # }
-#}
-
 AUTH_PASSWORD_VALIDATORS = [
    {
        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -183,7 +172,6 @@ USE_TZ = True
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'play2learn-bucket'
-# \
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_QUERYSTRING_AUTH = True
