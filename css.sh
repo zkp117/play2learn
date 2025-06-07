7d2671567f08366073cbd16c9d698d0a294c34a7
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-06-07 14:05:48 -0400

    pt.5

--- css.sh
+++ css.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Uploading scoreboard CSS to S3..."
+echo "Uploading CSS to S3..."
 aws s3 cp ./static/css/style.css s3://play2learn-bucket/css/style.css --acl public-read --content-type "text/css"
 
 # open this file in terminal
