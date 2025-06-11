830173a3c1c8358929f1f87d2483d99b31dd4b54
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-06-10 21:17:46 -0400

    pt.2

--- games/views.py
+++ games/views.py
@@ -15,10 +15,10 @@ from django.http import JsonResponse
 from django.views import View
 from datetime import timedelta
 @method_decorator(never_cache, name='dispatch')
-class MathFactsView(LoginRequiredMixin, TemplateView):
+class MathFactsView(TemplateView):
     template_name = "vue-templates/math-facts.html"
 @method_decorator(never_cache, name='dispatch')
-class AnagramHuntView(LoginRequiredMixin, TemplateView):
+class AnagramHuntView(TemplateView):
     template_name = "vue-templates/anagram-hunt.html"
 @method_decorator(login_required, name='dispatch')
 class EnterMathFactsScore(View):
