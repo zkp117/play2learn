fd3d9f0ea8e9860804ab18a587c4d0d2627e0868
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-06-10 21:55:27 -0400

    pt.9

--- vue-games/src/main.js
+++ vue-games/src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router'; 
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
--- vue-games/src/router.js
+++ vue-games/src/router.js
@@ -1,16 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import AnagramHunt from './apps/AnagramHunt.vue';
-import MathFacts from './apps/MathFacts.vue';
+// import { createRouter, createWebHistory } from 'vue-router';
+// import AnagramHunt from './apps/AnagramHunt.vue';
+// import MathFacts from './apps/MathFacts.vue';
 
-const routes = [
-  { path: '/vue-games/anagram-hunt', component: AnagramHunt },
-  { path: '/vue-games/math-facts', component: MathFacts },
+// const routes = [
+  // { path: '/vue-games/anagram-hunt', component: AnagramHunt },
+   // { path: '/vue-games/math-facts', component: MathFacts },
 
-];
+// ];
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
+// const router = createRouter({
+ // history: createWebHistory(),
+  // routes,
+// });
 
-export default router;
+// export default router;
