a58f8e3322bde4a65512aa4c2cf3565865628237
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-06-16 19:42:09 -0400

    pt.4

--- play2learn/urls.py
+++ play2learn/urls.py
@@ -6,13 +6,13 @@ from allauth.socialaccount.models import SocialToken, SocialAccount, SocialApp
 from users.views import homepage_view
 
 urlpatterns = [
-    path('users/', include('users.urls')),
+    path('', include('users.urls')),
     path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
     path('', homepage_view, name='home'),
     path('contact/', include('contact.urls', namespace='contact')),
-    path('pages/', include("pages.urls")),
-    path('reviews/', include('reviews.urls')),
+    path('', include("pages.urls")),
+    path('', include('reviews.urls')),
     path('scoreboards/', include('scoreboards.urls')),
 
     # routes for vue games
