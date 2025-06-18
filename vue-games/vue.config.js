4dcc05d2a189db3cfb8676729399cf241f28b833
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-06-18 18:51:18 -0400

    pt.11

--- vue-games/vue.config.js
+++ vue-games/vue.config.js
@@ -1,15 +1,13 @@
 module.exports = {
-  // This should be the **base URL path** your app is served from, relative to the domain
   publicPath: '/vue-games/',
-
   outputDir: 'dist',
-
-  // Only set indexPath like this if you want Vue build to output the index.html into a specific folder
-  // Usually you can leave this default, but if you want your Django templates to point to a static file, you can configure accordingly
   indexPath: 'index.html',
-
   filenameHashing: false,
   productionSourceMap: false,
+
+  css: {
+    extract: true  // <-- explicitly extract CSS to separate files
+  },
 
   configureWebpack: {
     devServer: {
