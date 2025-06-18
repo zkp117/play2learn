ec542c1adacbe444f58ac874e41371c5012c424e
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-06-18 19:29:05 -0400

    pt.15

--- vue-games/src/main.js
+++ vue-games/src/main.js
@@ -1,19 +1,17 @@
+// src/main.js or wherever your entry point is
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router'; 
+import router from './router';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
+// Configure Axios
 axios.defaults.xsrfCookieName = 'csrftoken';
-axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
 axios.defaults.withCredentials = true;
 
-const app = createApp(App);
-
-app.use(router); 
-app.use(VueAxios, axios);
-
-// DO NOT mount here!
-// app.mount('#app');
-
-window.vueApp = app;  // export app instance to global
+// Directly create and mount the Vue app
+createApp(App)
+  .use(router)
+  .use(VueAxios, axios)
+  .mount('#app');
