d5c62a88078018cdd7e4373b405ab7c70b33c9c0
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-06-18 13:54:32 -0400

    pt.3

--- games/urls.py
+++ games/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from .views import (
     RedirectMathFacts,
     RedirectAnagramHunt,
@@ -13,6 +14,8 @@ urlpatterns = [
     # These routes are meant to be accessed at /math-facts/ and /anagram-hunt/
     path('math-facts/', RedirectMathFacts.as_view(), name='math-facts'),
     path('anagram-hunt/', RedirectAnagramHunt.as_view(), name='anagram-hunt'),
+    path('anagram-hunt/', TemplateView.as_view(template_name='vue-templates/anagram-hunt.html')),
+    path('math-facts/', TemplateView.as_view(template_name='vue-templates/math-facts.html')),
 
     # These are API endpoints used by the Vue frontend
     path("api/record-score/mathfacts/", EnterMathFactsScore.as_view(), name="record_mathfacts_score"),
