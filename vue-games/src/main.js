ea4558642c33b1a6ba653e64a169f03d087d55e2
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-06-18 20:52:28 -0400

    pt.4

--- vue-games/src/main.js
+++ vue-games/src/main.js
@@ -4,16 +4,13 @@ import router from './router';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
-// Axios settings
+// Axios config
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
 axios.defaults.withCredentials = true;
 
-// Create app but DO NOT MOUNT
-const app = createApp(App);
-app.use(router);
-app.use(VueAxios, axios);
-
-// Export the app to global so Django template can mount it
-window.vueApp = app;
-
+// Create and mount app
+createApp(App)
+  .use(router)
+  .use(VueAxios, axios)
+  .mount('#app');
