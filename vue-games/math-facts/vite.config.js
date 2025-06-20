cfc988f434908a47b618bd31a8cf9b956ad8ac75
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-06-20 19:25:08 -0400

    pt.13

--- vue-games/math-facts/vite.config.js
+++ vue-games/math-facts/vite.config.js
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/vue-games/math-facts/',
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `js/app.js`,
+        chunkFileNames: `js/chunk-vendors.js`,
+        assetFileNames: `assets/[name].[ext]`,
+      }
+    }
+  }
 })
--- vue-games/vue.sh
+++ vue-games/vue.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+cd "$(dirname "$0")"
+
 echo "Building Math Facts game..."
 cd math-facts
 npm install
