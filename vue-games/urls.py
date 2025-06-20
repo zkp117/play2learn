1ed843e6ac5645e586518a11e85310bf3ac1bf9f
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-06-19 22:22:50 -0400

    pt.9

--- vue-games/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-app_name = 'vue-games'
-
-urlpatterns = [
-    path('math-facts/', TemplateView.as_view(template_name="vue-templates/math-facts.html"), name='math-facts'),
-    path('anagram-hunt/', TemplateView.as_view(template_name="vue-templates/anagram-hunt.html"), name='anagram-hunt'),
-]
