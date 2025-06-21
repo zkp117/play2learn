47053f288de816a3f9c3e84953e0ca311fb816e0
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-06-20 23:32:17 -0400

    pt.17

--- vue-games/anagram-hunt/vite.config.js
+++ vue-games/anagram-hunt/vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/vue-games/anagram-hunt/', // or math-facts/
+  base: '/vue-games/anagram-hunt/',
   plugins: [vue()],
   build: {
-    target: 'es2015',  // ✅ Ensures no raw "export" syntax in JS output
     rollupOptions: {
       output: {
         entryFileNames: `js/app.js`,
         chunkFileNames: `js/chunk-vendors.js`,
-        assetFileNames: `assets/[name].[ext]`,
+        assetFileNames: `assets/[name].[ext]`
       }
     }
   }
