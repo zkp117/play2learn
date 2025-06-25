17e1ce5cf643e890cdd7e3fd751f82195520fb4c
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-06-24 20:12:00 -0400

    pt.10

--- vue-games/anagram-hunt/src/apps/AnagramHunt.vue
+++ vue-games/anagram-hunt/src/apps/AnagramHunt.vue
@@ -2,27 +2,31 @@
   <div class="container" style="width: 500px">
 
 <!-- Modal for Login Prompt -->
+<!-- Modal for Login Prompt -->
 <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content rounded-4 shadow">
       <div class="modal-header p-5 pb-4 border-bottom-0">
-        <h1 class="fw-bold mb-0 fs-2">Sign up for free</h1>
+        <h1 class="fw-bold mb-0 fs-2">Log in</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body p-5 pt-0">
-        <form>
+        <form method="post" action="/accounts/login/">
           <div class="form-floating mb-3">
-            <input type="email" class="form-control rounded-3" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
+            <input type="email" class="form-control rounded-3" id="loginEmail" name="login" placeholder="name@example.com">
+            <label for="loginEmail">Email address</label>
           </div>
           <div class="form-floating mb-3">
-            <input type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
+            <input type="password" class="form-control rounded-3" id="loginPassword" name="password" placeholder="Password">
+            <label for="loginPassword">Password</label>
           </div>
-          <a href="/accounts/signup/" class="btn btn-primary w-100 mb-2 btn-lg rounded-3">Sign up</a>
-          <small class="text-body-secondary">By clicking Sign up, you agree to the terms of use.</small>
-          <hr class="my-4">
+          <button type="submit" class="w-100 mb-2 btn btn-lg rounded-3 btn-primary">Log in</button>
         </form>
+
+        <div class="text-center mt-3">
+          <small class="text-muted">No account?</small>
+          <a href="/accounts/signup/" class="btn btn-outline-secondary w-100 mt-2 rounded-3">Sign up</a>
+        </div>
       </div>
     </div>
   </div>
