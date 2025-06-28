47ad61938c10215e615f133844c8eaa8c9243c8f
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-06-28 11:12:32 -0400

    pt.pt.3

--- vue-games/math-facts/vite.config.js
+++ vue-games/math-facts/vite.config.js
@@ -2,15 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/vue-games/math-facts/',  // or '/vue-games/anagram-hunt/' in the other config
+  base: '/vue-games/math-facts/',
   plugins: [vue()],
   build: {
+    outDir: 'dist',
+    manifest: true, 
     rollupOptions: {
+      input: './src/main.js',
       output: {
-        entryFileNames: `js/app.js`,
-        // remove chunkFileNames to avoid chunk-vendors.js
-        // chunkFileNames: `js/chunk-vendors.js`, <-- remove/comment this
-        assetFileNames: `assets/[name][extname]`
+        entryFileNames: 'js/[name].[hash].js',
+        chunkFileNames: 'js/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash][extname]'
       }
     }
   }
