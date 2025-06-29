3fa8a7bf63100eeb99ae64b57f70d17d2bd10e8b
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-06-29 18:58:39 -0400

    pt.5

--- play2learn/context_processors.py
+++ play2learn/context_processors.py
@@ -1,6 +1,6 @@
-## from django.conf import settings
+from django.conf import settings
 
-### def vue_games_cdn(request):
-  ##  return {
-      ##  'VUE_GAMES_CDN_URL': settings.VUE_GAMES_CDN_URL,
-  ##  }
+def vue_games_cdn(request):
+    return {
+        'VUE_GAMES_CDN_URL': settings.VUE_GAMES_CDN_URL,
+    }
